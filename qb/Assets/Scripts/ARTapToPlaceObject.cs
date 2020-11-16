7d568fbd5c9981e318bdb86b49c8f2fe19fd418e
Jorge Ruiz <jorgerv97@gmail.com>
2020-11-16 17:53:13 +0100

    Template project

---
+++ qb/Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject gameObjectToInstantiate;
+
+    private GameObject spawnedObject;
+    private ARRaycastManager _arRaycastManager;
+    private Vector2 touchPosition;
+    private AREnvironmentProbeManager _arProbeMgr;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+        _arProbeMgr = GetComponent<AREnvironmentProbeManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition)) return;
+
+        if(_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+
+            if(spawnedObject == null)
+            {
+                //_arProbeMgr.AddEnvironmentProbe(hitPose, new Vector3(10f, 10f, 10f), new Vector3(10f, 10f, 10f));
+                spawnedObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                //_arProbeMgr.AddEnvironmentProbe(hitPose, new Vector3(10f, 10f, 10f), new Vector3(10f, 10f, 10f));
+                spawnedObject.transform.position = hitPose.position;
+                spawnedObject.transform.rotation = hitPose.rotation;
+            }
+        }
+    }
+}
---
+++ qb/Assets/Scripts/LightEstimation.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+
+public class LightEstimation : MonoBehaviour
+{
+    [SerializeField]
+    private ARCameraManager arCameraManager;
+
+    public Text BrightnessValue;
+    public Text TempValue;
+    public Text ColorCorrValue;
+
+    private Light mLight;
+
+    private void Awake()
+    {
+        mLight = GetComponent<Light>();
+    }
+
+    private void OnEnable()
+    {
+        arCameraManager.frameReceived += FrameUpdated;
+    }
+
+    private void OnDisable()
+    {
+        arCameraManager.frameReceived -= FrameUpdated;
+    }
+
+    private void FrameUpdated(ARCameraFrameEventArgs args)
+    {
+        if (args.lightEstimation.averageBrightness.HasValue)
+        {
+            BrightnessValue.text = "Brightness: " + args.lightEstimation.averageBrightness.Value;
+            mLight.intensity = args.lightEstimation.averageBrightness.Value + 0.3f;
+        }
+
+        if (args.lightEstimation.averageColorTemperature.HasValue)
+        {
+            TempValue.text = "Temperature: " + args.lightEstimation.averageColorTemperature.Value;
+            mLight.colorTemperature = args.lightEstimation.averageColorTemperature.Value;
+        }
+
+        if (args.lightEstimation.colorCorrection.HasValue)
+        {
+            ColorCorrValue.text = "Color Correction: " + args.lightEstimation.colorCorrection.Value;
+            mLight.color = args.lightEstimation.colorCorrection.Value;
+        }
+    }
+}
