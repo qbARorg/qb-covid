9340e6459f33c791a060bb48e172a2f37123af51
Jorge Ruiz <jorgerv97@gmail.com>
2020-12-21 14:23:20 +0100

    Jorge first part

---
+++ qb/Assets/Scripts/Classroom/ClassroomCanvasController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ClassRoom
+{
+    public class ClassroomCanvasController : MonoBehaviour
+    {
+        private ClassroomController mainController = null;
+        [SerializeField]
+        private Text doorButtonText;
+        [SerializeField]
+        private string doorClosedtext = "Door: Closed";
+        [SerializeField]
+        private string doorOpenedtext = "Door: Opened";
+        [SerializeField]
+        private ParticleSystem doorParticles;
+
+        [SerializeField]
+        private Text window1ButtonText;
+        [SerializeField]
+        private string window1Closedtext = "Window 1: Closed";
+        [SerializeField]
+        private string window1Openedtext = "Window 1: Opened";
+        [SerializeField]
+        private ParticleSystem w1Particles;
+
+        [SerializeField]
+        private Text window2ButtonText;
+        [SerializeField]
+        private string window2Closedtext = "Window 2: Closed";
+        [SerializeField]
+        private string window2Openedtext = "Window 2: Opened";
+        [SerializeField]
+        private ParticleSystem w2Particles;
+
+        [SerializeField]
+        private Text window3ButtonText;
+        [SerializeField]
+        private string window3Closedtext = "Window 3: Closed";
+        [SerializeField]
+        private string window3Openedtext = "Window 3: Opened";
+        [SerializeField]
+        private ParticleSystem w3Particles;
+
+        private void Start()
+        {
+            mainController = this.GetComponent<ClassroomController>();
+            doorButtonText.text = doorClosedtext;
+            window1ButtonText.text = window1Closedtext;
+            window2ButtonText.text = window2Closedtext;
+            window3ButtonText.text = window3Closedtext;
+            doorParticles.Stop();
+            w1Particles.Stop();
+            w2Particles.Stop();
+            w3Particles.Stop();
+        }
+
+        public void doorButtonClick()
+        {
+            if(mainController.getDoorState())
+            {
+                mainController.setDoorState(false);
+                doorButtonText.text = doorClosedtext;
+                doorParticles.Stop();
+            }
+            else
+            {
+                mainController.setDoorState(true);
+                doorButtonText.text = doorOpenedtext;
+                doorParticles.Play();
+            }
+        }
+
+        public void window1ButtonClick()
+        {
+            if (mainController.getWindow1State())
+            {
+                mainController.setWindow1State(false);
+                window1ButtonText.text = window1Closedtext;
+                w1Particles.Stop();
+            }
+            else
+            {
+                mainController.setWindow1State(true);
+                window1ButtonText.text = window1Openedtext;
+                w1Particles.Play();
+            }
+        }
+
+        public void window2ButtonClick()
+        {
+            if (mainController.getWindow2State())
+            {
+                mainController.setWindow2State(false);
+                window2ButtonText.text = window2Closedtext;
+                w2Particles.Stop();
+            }
+            else
+            {
+                mainController.setWindow2State(true);
+                window2ButtonText.text = window2Openedtext;
+                w2Particles.Play();
+            }
+        }
+
+        public void window3ButtonClick()
+        {
+            if (mainController.getWindow3State())
+            {
+                mainController.setWindow3State(false);
+                window3ButtonText.text = window3Closedtext;
+                w3Particles.Stop();
+            }
+            else
+            {
+                mainController.setWindow3State(true);
+                window3ButtonText.text = window3Openedtext;
+                w3Particles.Play();
+            }
+        }
+    }
+}
---
+++ qb/Assets/Scripts/Classroom/ClassroomController.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClassRoom
+{
+    public class ClassroomController : MonoBehaviour
+    {
+        #region Attibutes
+        [Header("Control Variables")]
+        [SerializeField]
+        private bool doorOpen = false;
+        [SerializeField]
+        private bool window1Open = false;
+        [SerializeField]
+        private bool window2Open = false;
+        [SerializeField]
+        private bool window3Open = false;
+        [Range(0f, 1f)]
+        private float doorLerpState = 0f;
+        [Range(0f, 1f)]
+        private float window1LerpState = 0f;
+        [Range(0f, 1f)]
+        private float window2LerpState = 0f;
+        [Range(0f, 1f)]
+        private float window3LerpState = 0f;
+        public float animationsDuration = 1f;
+
+        [Header("Door")]
+        public GameObject doorHinge;
+        public float doorClosedAngle = 0f;
+        public float doorOpenedAngle = 0f;
+        [Header("Window 1")]
+        public GameObject window11Hinge;
+        public float window11ClosedAngle = 0f;
+        public float window11OpenedAngle = 0f;
+        public GameObject window12Hinge;
+        public float window12ClosedAngle = 0f;
+        public float window12OpenedAngle = 0f;
+        [Header("Window 2")]
+        public GameObject window21Hinge;
+        public float window21ClosedAngle = 0f;
+        public float window21OpenedAngle = 0f;
+        public GameObject window22Hinge;
+        public float window22ClosedAngle = 0f;
+        public float window22OpenedAngle = 0f;
+        [Header("Window 3")]
+        public GameObject window31Hinge;
+        public float window31ClosedAngle = 0f;
+        public float window31OpenedAngle = 0f;
+        public GameObject window32Hinge;
+        public float window32ClosedAngle = 0f;
+        public float window32OpenedAngle = 0f;
+
+        [Header("Performance or Smooth anims")]
+        public bool performanceMode = false;
+        #endregion
+
+        #region Propperties
+        public bool getDoorState()
+        {
+            return doorOpen;
+        }
+        public void setDoorState(bool newState)
+        {
+            doorOpen = newState;
+        }
+
+        public bool getWindow1State()
+        {
+            return window1Open;
+        }
+        public void setWindow1State(bool newState)
+        {
+            window1Open = newState;
+        }
+
+        public bool getWindow2State()
+        {
+            return window2Open;
+        }
+        public void setWindow2State(bool newState)
+        {
+            window2Open = newState;
+        }
+
+        public bool getWindow3State()
+        {
+            return window3Open;
+        }
+        public void setWindow3State(bool newState)
+        {
+            window3Open = newState;
+        }
+        #endregion
+
+        #region Main Methods
+        void Update()
+        {
+            doorController();
+            window1Controller();
+            window2Controller();
+            window3Controller();
+        }
+        #endregion
+
+        #region Custom Methods
+        public void doorController()
+        {
+            if(doorOpen && doorLerpState < 1)
+            {
+                doorLerpState += Time.deltaTime / animationsDuration;
+            }
+            else if(!doorOpen && doorLerpState > 0)
+            {
+                doorLerpState -= Time.deltaTime / animationsDuration;
+            }
+
+            if(!performanceMode)
+            {
+                float newAngle = (doorOpenedAngle - doorClosedAngle) / 2f - (doorOpenedAngle - doorClosedAngle) / 2f * Mathf.Cos(doorLerpState * Mathf.PI);
+                doorHinge.transform.eulerAngles = new Vector3(0, newAngle, 0);
+            }
+            else doorHinge.transform.eulerAngles = Vector3.Slerp(new Vector3(0f, doorClosedAngle, 0f), new Vector3(0f, doorOpenedAngle, 0f), doorLerpState);
+        }
+
+        public void window1Controller()
+        {
+            if (window1Open && window1LerpState < 1)
+            {
+                window1LerpState += Time.deltaTime / animationsDuration;
+            }
+            else if (!window1Open && window1LerpState > 0)
+            {
+                window1LerpState -= Time.deltaTime / animationsDuration;
+            }
+
+            if (!performanceMode)
+            {
+                float newAngle1 = (window11OpenedAngle - window11ClosedAngle) / 2f - (window11OpenedAngle - window11ClosedAngle) / 2f * Mathf.Cos(window1LerpState * Mathf.PI);
+                window11Hinge.transform.eulerAngles = new Vector3(0, newAngle1, 0);
+                float newAngle2 = (window12OpenedAngle - window12ClosedAngle) / 2f - (window12OpenedAngle - window12ClosedAngle) / 2f * Mathf.Cos(window1LerpState * Mathf.PI);
+                window12Hinge.transform.eulerAngles = new Vector3(0, newAngle2, 0);
+            }
+            else
+            {
+                window11Hinge.transform.eulerAngles = Vector3.Slerp(new Vector3(0f, window11ClosedAngle, 0f), new Vector3(0f, window11OpenedAngle, 0f), window1LerpState);
+                window12Hinge.transform.eulerAngles = Vector3.Slerp(new Vector3(0f, window12ClosedAngle, 0f), new Vector3(0f, window12OpenedAngle, 0f), window1LerpState);
+            }
+        }
+
+        public void window2Controller()
+        {
+            if (window2Open && window2LerpState < 1)
+            {
+                window2LerpState += Time.deltaTime / animationsDuration;
+            }
+            else if (!window2Open && window2LerpState > 0)
+            {
+                window2LerpState -= Time.deltaTime / animationsDuration;
+            }
+
+            if (!performanceMode)
+            {
+                float newAngle1 = (window21OpenedAngle - window21ClosedAngle) / 2f - (window21OpenedAngle - window21ClosedAngle) / 2f * Mathf.Cos(window2LerpState * Mathf.PI);
+                window21Hinge.transform.eulerAngles = new Vector3(0, newAngle1, 0);
+                float newAngle2 = (window22OpenedAngle - window22ClosedAngle) / 2f - (window22OpenedAngle - window22ClosedAngle) / 2f * Mathf.Cos(window2LerpState * Mathf.PI);
+                window22Hinge.transform.eulerAngles = new Vector3(0, newAngle2, 0);
+            }
+            else
+            {
+                window21Hinge.transform.eulerAngles = Vector3.Slerp(new Vector3(0f, window21ClosedAngle, 0f), new Vector3(0f, window21OpenedAngle, 0f), window2LerpState);
+                window22Hinge.transform.eulerAngles = Vector3.Slerp(new Vector3(0f, window22ClosedAngle, 0f), new Vector3(0f, window22OpenedAngle, 0f), window2LerpState);
+            }
+            
+        }
+
+        public void window3Controller()
+        {
+            if (window3Open && window3LerpState < 1)
+            {
+                window3LerpState += Time.deltaTime / animationsDuration;
+            }
+            else if (!window3Open && window3LerpState > 0)
+            {
+                window3LerpState -= Time.deltaTime / animationsDuration;
+            }
+
+            if (!performanceMode)
+            {
+                float newAngle1 = (window31OpenedAngle - window31ClosedAngle) / 2f - (window31OpenedAngle - window31ClosedAngle) / 2f * Mathf.Cos(window3LerpState * Mathf.PI);
+                window31Hinge.transform.eulerAngles = new Vector3(0, newAngle1, 0);
+                float newAngle2 = (window32OpenedAngle - window32ClosedAngle) / 2f - (window32OpenedAngle - window32ClosedAngle) / 2f * Mathf.Cos(window3LerpState * Mathf.PI);
+                window32Hinge.transform.eulerAngles = new Vector3(0, newAngle2, 0);
+            }
+            else
+            {
+                window31Hinge.transform.eulerAngles = Vector3.Slerp(new Vector3(0f, window31ClosedAngle, 0f), new Vector3(0f, window31OpenedAngle, 0f), window1LerpState);
+                window32Hinge.transform.eulerAngles = Vector3.Slerp(new Vector3(0f, window32ClosedAngle, 0f), new Vector3(0f, window32OpenedAngle, 0f), window1LerpState);
+            }
+        }
+        #endregion
+    }
+
+}
