8172ad7a7c455d7d1949eba2898661a1ee42bf77
adribasterra <adrianabasterraa@gmail.com>
2021-01-07 15:07:15 +0100

    Fix

--- qb/Assets/Scripts/1-Hands/ARShooting.cs
+++ qb/Assets/Scripts/1-Hands/ARShooting.cs
@@ -9,88 +9,64 @@ using UnityEngine.XR.ARSubsystems;
 public class ARShooting : MonoBehaviour
 {
     #region Attributes
-
+    [Header("Prefabs")]
     public GameObject handsPrefab;
     public GameObject gelPrefab;
-    public GameObject ballParent;
+    public GameObject gelParent;
     public GameObject scenePrefab;
 
-    [SerializeField] [Range(0.001f, 0.1f)] float distanceBtwDots;
-
     private GameObject scene = null;
-    private GameObject hands;
-    private GameObject gel;
     private GameObject gelBottle;
     private GameObject gelBottleHead;
     private Transform shootPosition;
-    private Vector3 offsetBottlePos;
-    private bool once = true;
 
-    private GameObject[] points;
+    [Header("Bottle animation")]
+    private Vector3 offsetBottlePos;
+    private enum Animate { up = 1, down = 0 };
+    private bool finishDownAnimation = false;
+
+    [Header("Gel")]
     public int numPoints;
-    public float force = 1.0f;
+    private GameObject[] points;
+    [SerializeField] [Range(4f, 10f)] private float force = 6.0f;
+    [SerializeField] [Range(0.001f, 0.1f)] private float distanceBtwPoints;
 
     private Vector3 startPoint;
     private Vector3 endPoint;
     private Vector3 direction;
-    private Vector3 force3;
-    private float distance;
+
     private bool isDragging;
 
     private ARTrackedImageManager imageManager;
-
-    #region Trash Att
-    private GameObject ARCam;
-
-    private LineRenderer line;
-    private LayerMask layer;
-    private int lineSegment;
-    private float flightTime = 1.0f;
-    private Vector3 height;
-
-    private Vector2 touchPosition;
-
-    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    private ARRaycastManager _arRaycastManager;
-
-    #endregion
+    
     #endregion
 
     // Start is called before the first frame update
     private void Awake()
     {
-        imageManager = this.GetComponent<ARTrackedImageManager>();
         offsetBottlePos = new Vector3(0f, 0.1f, 0f);
-
-        imageManager.trackedImagesChanged += OnChanged;
 
         points = new GameObject[numPoints];
         for(int i = 0; i<numPoints; i++)
         {
             points[i] = Instantiate(gelPrefab, transform.position, Quaternion.identity);
-            points[i].transform.SetParent(ballParent.transform);
+            points[i].transform.SetParent(gelParent.transform);
         }
     }
 
     private void OnDragStart()
     {
-        startPoint = Input.mousePosition;
-        if(Input.touchCount > 0) startPoint = Input.GetTouch(0).position;
+        endPoint = Input.mousePosition;
+        endPoint.z = 10;
+        endPoint = Camera.main.ScreenToWorldPoint(endPoint);
+        if(Input.touchCount > 0) endPoint = Input.GetTouch(0).position;
     }
 
     private void OnDrag()
     {
-        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(camRay, out hit, 20f))
-        {
-            endPoint = hit.point;
-            distance = Vector3.Distance(startPoint, endPoint);
-            direction = (startPoint - endPoint).normalized;
-            force3 = direction * distance * force;
-        }
+        direction = endPoint - startPoint;
         //Debug
+        Debug.Log($"start {startPoint}, end {endPoint}");
         Debug.DrawLine(startPoint, endPoint);
     }
 
@@ -106,81 +82,38 @@ public class ARShooting : MonoBehaviour
     {
         if (!scene)
         {
-            scene = GameObject.FindGameObjectWithTag("MainCamera").transform.GetChild(0).gameObject;
+            scene = Camera.main.transform.GetChild(0).gameObject;
             if (scene != null) {
                 SetVariables();
             }
-        }
-        if (!hands)
-        {
-            hands = Instantiate(handsPrefab, handsPrefab.transform.position, handsPrefab.transform.rotation);
-            //hands.transform.parent = GameObject.FindGameObjectWithTag("MainCamera").transform;
         }
         if (Input.GetMouseButton(0) || Input.touchCount > 0)
         {
             isDragging = true;
             OnDragStart();
-            if (once)
-            {
-                gelBottleHead.transform.position -= offsetBottlePos;
-                once = false;
-            }
+
+            //Animate bottle
+            if (!finishDownAnimation) AnimateBottle(Animate.down);
         }
 
-        if(Application.platform == RuntimePlatform.Android)
+        if (Input.GetMouseButtonUp(0)) /*if (Input.touchCount <= 0)*/
         {
-            if(Input.touchCount <= 0)
-            {
-                if (!once)
-                {
-                    gelBottleHead.transform.position += offsetBottlePos;
-                    once = true;
-                    isDragging = false;
-                }
-            }
-        }
-        else
-        {
-            if (Input.GetMouseButtonUp(0))
-            {
-                if (!once)
-                {
-                    gelBottleHead.transform.position += offsetBottlePos;
-                    once = true;
-                    isDragging = false;
-                }
-            }
+            if (finishDownAnimation) AnimateBottle(Animate.up);
+            isDragging = false;
         }
 
         if (isDragging)
         {
-            ballParent.SetActive(true);
-            //Vector3 d = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //gelBottle.transform.position = new Vector3(d.x, gelBottle.transform.position.y, gelBottle.transform.position.z);
+            //Show points
+            gelParent.SetActive(true);
             OnDrag();
+            //Calculate each points' position
             for (int i = 0; i < numPoints; i++)
             {
-                points[i].transform.position = PosInTime(i * distanceBtwDots);
+                points[i].transform.position = PosInTime(i * distanceBtwPoints);
             }
         }
-        else
-        {
-            ballParent.SetActive(false);
-        }
-
-    }
-
-    void OnDisable() => imageManager.trackedImagesChanged -= OnChanged;
-
-    void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
-    {
-        foreach (var trackedImage in eventArgs.added)
-        {
-            if (trackedImage.trackingState != TrackingState.None)
-            {
-                scene.SetActive(true);
-            }
-        }
+        else gelParent.SetActive(false);
     }
 
     public Vector3 PosInTime(float t)
@@ -188,116 +121,17 @@ public class ARShooting : MonoBehaviour
         return shootPosition.position + (direction.normalized * force * t) + 0.5f * Physics.gravity * t * t;
     }
 
-    #region Trash Meth
-    //Update()
-    //{
-    //if(Input.touchCount > 0 || Input.GetMouseButtonDown(0))
-    //{
-    //    height = Vector3.Lerp(Input.mousePosition, handsPrefab.transform.position, 0.5f);
-    //}
-
-    //height is min (0f, 0.5f, 0f);
-
-    ////if (!TryGetTouchPosition(out Vector2 touchPosition)) return;
-
-    //if (Input.touchCount > 0 || Input.GetMouseButton(0))
-    //{
-
-    //    Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //    RaycastHit hit;
-
-    //    if (Physics.Raycast(camRay, out hit, 100f, layer))
-    //    {
-
-    //        Vector3 initialVelocity = CalculateVelocty(hit.point, shootPosition.position, flightTime);
-
-    //        Visualize(initialVelocity, hit.point + Vector3.up * 0.1f); //we include the cursor position as the final nodes for the line visual position
-
-    //        transform.rotation = Quaternion.LookRotation(initialVelocity);
-    //    }
-
-    //    //Vector3.MoveTowards(gelBottleHead.transform.position, gelBottleHead.transform.position - new Vector3(0f, 0.1f, 0f), 0.01f * Time.deltaTime);
-    //    if (once)
-    //    {
-    //        gelBottleHead.transform.position -= offsetBottlePos;
-    //        once = false;
-    //    }
-    //    //Shooting
-    //    if(gel == null)
-    //    {
-    //        gel = Instantiate(gelPrefab, gelPrefab.transform.position, gelPrefab.transform.rotation);
-    //        gel.GetComponent<GelMovement>().shootPosition = shootPosition;
-    //    }
-
-    //}
-    //else
-    //{
-    //    gelBottleHead.transform.position = gelBottleHead.transform.parent.transform.position;
-    //    once = true;
-    //}
-
-    //if (_arRaycastManager.Raycast(touchPosition, hits))
-    //{
-    //    var hitPose = hits[0].pose;
-    //    Vector3 touchWorldPosition = Camera.main.ScreenToWorldPoint(touchPosition);
-
-    //}
-    //}
-
-
-    bool TryGetTouchPosition(out Vector2 touchPosition)
+    private void AnimateBottle(Animate direction)
     {
-        if (Input.touchCount > 0)
+        if (direction == Animate.up)
         {
-            touchPosition = Input.GetTouch(0).position;
-            return true;
+            gelBottleHead.transform.position += offsetBottlePos;
+            finishDownAnimation = false;
         }
-        touchPosition = default;
-        return false;
-    }
-
-    private void Visualize(Vector3 initialVel, Vector3 finalPos)
-    {
-        for (int i = 0; i < lineSegment; i++)
+        else if (direction == Animate.down)
         {
-            Vector3 pos = CalculatePosInTime(initialVel, (i / (float)lineSegment) * flightTime);
-            line.SetPosition(i, pos);
+            gelBottleHead.transform.position -= offsetBottlePos;
+            finishDownAnimation = true;
         }
-
-        line.SetPosition(lineSegment, finalPos);
     }
-
-
-    private Vector3 CalculateVelocty(Vector3 target, Vector3 origin, float time)
-    {
-        Vector3 distance = target - origin;
-        Vector3 distance2D = new Vector3(distance.x, 0f, distance.z);
-
-        float sY = distance.y;
-        float sXZ = distance2D.magnitude;
-
-        float vXZ = sXZ / time;
-        float vY = (sY / time) + (0.5f * Mathf.Abs(Physics.gravity.y) * time);
-
-        Vector3 result = distance2D.normalized;
-        result *= vXZ;
-        result.y = vY;
-
-        return result;
-    }
-
-    private Vector3 CalculatePosInTime(Vector3 initialVel, float time)
-    {
-        Vector3 vXZ = initialVel;
-        vXZ.y = 0f;
-
-        Vector3 result = shootPosition.position + initialVel * time;
-        float sY = (-0.5f * Mathf.Abs(Physics.gravity.y) * (time * time)) + (initialVel.y * time) + shootPosition.position.y;
-
-        result.y = sY;
-
-        return result;
-    }
-
-    #endregion
 }
--- qb/Assets/Scripts/1-Hands/GelMovement.cs
+++ qb/Assets/Scripts/1-Hands/GelMovement.cs
@@ -23,35 +23,4 @@ public class GelMovement : MonoBehaviour
     {
         
     }
-
-    #region Trash
-
-    //private float bulletSpeed = 100.0f;
-    //private float height = 10f;
-    //private Vector3 initalVel = new Vector3(0f, 1.0f, 1.0f);
-    //private Transform shootPosition;
-    //shootPosition = GameObject.FindWithTag("BottleHead").transform.GetChild(0).transform;
-    //Calculate angle depending on screenPos
-
-    //this.transform.position = ParabolicTrajectory(shootPosition.position, shootPosition.position + new Vector3(0, 15f, 15f), height, 1.0f * Time.deltaTime);
-
-    //this.transform.localScale += new Vector3(0f, 0f, 0.1f * Time.deltaTime);
-    //this.transform.position = _ARShooting.PosInTime(Time.deltaTime);
-    //v = v0 cos(a)i + v0 sin(a)j;
-    //a = -gj
-
-    //this.transform.position += new Vector3(0f, -4.9f * Time.deltaTime * Time.deltaTime + initalVel.y * Time.deltaTime, initalVel.z * Time.deltaTime);
-
-
-    //r(t) = (v0x * t + x0)i + (-1/2 * g * t * t + v0y * t + y0)j
-    //private static Vector3 ParabolicTrajectory(Vector3 startPoint, Vector3 endPoint, float height, float time)
-    //{
-    //    Func<float, float> Function = x => -4 * height * x * x + 4 * height * x;
-
-    //    var middlePoint = Vector3.Lerp(startPoint, endPoint, time);
-
-    //    return new Vector3(middlePoint.x, Function(time) + Mathf.Lerp(startPoint.y, endPoint.y, time), middlePoint.z);
-    //}
-
-    #endregion
 }
