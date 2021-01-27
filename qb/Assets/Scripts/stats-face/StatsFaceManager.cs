06ba402d1088f5a6506989901f245aefc7123fe8
gabivlj <gabitriqui@gmail.com>
2021-01-27 01:17:34 +0000

    credits

---
+++ qb/Assets/Scripts/stats-face/StatsFaceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class StatsFaceManager : TrackerListener
+{
+    [SerializeField]
+    private TextMesh text;
+    private TextMesh textInstance;
+    
+    public override void OnDetectedStart(ARTrackedImage img)
+    {
+        textInstance = Instantiate(text, img.transform);
+    }
+
+    public override void OnStoppingDetection()
+    {
+            
+    }
+
+    public override void ARUpdate()
+    {
+        base.ARUpdate();
+    }
+}
